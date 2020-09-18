d1cd1368fcd8768c4c8fbb22976cb2db51f834aa
Aakash Agarwal <54284599+Aakash310@users.noreply.github.com>
2020-09-18 12:26:48 +0530

    Add files via upload

---
+++ image_editing.py
@@ -0,0 +1,29 @@
+from PIL import Image, ImageEnhance , ImageFilter
+
+#img = Image.open('Capture.PNG')
+#img.save('ID.pdf')
+#img.show()
+
+#MAX_SIZE = (250,250)
+#img.thumbnail(MAX_SIZE)
+#img.save('ed2_1.jpeg')
+#img.show()
+
+#enhancer = ImageEnhance.Sharpness(img)
+#enhancer.enhance(0).save('ed2_2.jpeg')
+
+#enhancer = ImageEnhance.Color(img)
+#enhancer.enhance(0).save('ed2_2.jpeg')
+
+#enhancer = ImageEnhance.Brightness(img)
+#enhancer.enhance(0).save('ed2_2.jpeg')
+
+#enhancer = ImageEnhance.Contrast(img)
+#enhancer.enhance(0).save('ed2_2.jpeg')
+
+#filters = ImageFilter.GaussianBlur(img)
+#filters.filter(0).save('ed2.jpeg')
+
+
+
+
